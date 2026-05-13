319434c01b5d3a332266ca41b32e748332b9bdf1
Amzar Natsir <53068228+AmzarNatsir@users.noreply.github.com>
2026-05-13 17:08:39 +0800

    update gambar halaman login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
     <!-- ========================
-        Start Page Content
-    ========================= -->
+                    Start Page Content
+                ========================= -->
 
     <div class="overflow-hidden p-3 acc-vh">
 
@@ -17,10 +17,12 @@
                 <div class="row">
 
                     <div class="col-md-10 mx-auto">
-                        <form action="{{ route('login') }}" method="POST" class=" vh-100 d-flex justify-content-between flex-column p-4 pb-0">
+                        <form action="{{ route('login') }}" method="POST"
+                            class=" vh-100 d-flex justify-content-between flex-column p-4 pb-0">
                             @csrf
                             <div class="text-center mb-4 auth-logo">
-                                <img src="{{URL::asset('assets/logo_perusahaan/logo_ssb.png')}}" class="img-fluid" alt="Logo">
+                                <img src="{{URL::asset('assets/logo_perusahaan/logo_ssb.png')}}" class="img-fluid"
+                                    alt="Logo">
                             </div>
                             <div>
                                 <div class="mb-3">
@@ -30,7 +32,9 @@
                                 <div class="mb-3">
                                     <label class="form-label">NIK</label>
                                     <div class="input-group input-group-flat">
-                                        <input type="text" name="nik" class="form-control @error('nik') is-invalid @enderror" value="{{ old('nik') }}" required autofocus>
+                                        <input type="text" name="nik"
+                                            class="form-control @error('nik') is-invalid @enderror" value="{{ old('nik') }}"
+                                            required autofocus>
                                         <span class="input-group-text">
                                             <i class="ti ti-mail"></i>
                                         </span>
@@ -44,7 +48,9 @@
                                 <div class="mb-3">
                                     <label class="form-label">Password</label>
                                     <div class="input-group input-group-flat pass-group">
-                                        <input type="password" name="password" class="form-control pass-input @error('password') is-invalid @enderror" required>
+                                        <input type="password" name="password"
+                                            class="form-control pass-input @error('password') is-invalid @enderror"
+                                            required>
                                         <span class="input-group-text toggle-password ">
                                             <i class="ti ti-eye-off"></i>
                                         </span>
@@ -60,7 +66,9 @@
                                 </div>
                             </div>
                             <div class="text-center pb-4">
-                                <p class="text-dark mb-0">Copyright &copy; <script>document.write(new Date().getFullYear())</script> - SSB 2.0</p>
+                                <p class="text-dark mb-0">Copyright &copy;
+                                    <script>document.write(new Date().getFullYear())</script> - SSB 2.0
+                                </p>
                             </div>
                         </form>
                     </div> <!-- end col -->
@@ -78,7 +86,7 @@
     </div>
 
     <!-- ========================
-        End Page Content
-    ========================= -->
+                    End Page Content
+                ========================= -->
 
 @endsection
